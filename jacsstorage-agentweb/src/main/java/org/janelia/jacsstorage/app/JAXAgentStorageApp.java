1411ae2b2f706913bc5330887430d2a88cfcdd84
Cristian Goina <goinac@janelia.hhmi.org>
2017-10-16 13:28:32 -0400

    forgot to register the agent storage resource with the jax rs application

--- jacsstorage-agentweb/src/main/java/org/janelia/jacsstorage/app/JAXAgentStorageApp.java
+++ jacsstorage-agentweb/src/main/java/org/janelia/jacsstorage/app/JAXAgentStorageApp.java
@@ -2,12 +2,16 @@ package org.janelia.jacsstorage.app;
 
 import com.google.common.collect.ImmutableSet;
 import org.janelia.jacsstorage.rest.AgentConnectionResource;
+import org.janelia.jacsstorage.rest.AgentStorageResource;
 
 import java.util.Set;
 
 public class JAXAgentStorageApp extends AbstractJAXApp {
     @Override
     protected Set<Class<?>> getAppClasses() {
-        return ImmutableSet.of(AgentConnectionResource.class);
+        return ImmutableSet.of(
+                AgentConnectionResource.class,
+                AgentStorageResource.class
+        );
     }
 }
