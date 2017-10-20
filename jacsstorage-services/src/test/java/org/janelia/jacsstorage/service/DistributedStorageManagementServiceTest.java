f64824bca9b88ca89ea94f13d1a1a7e2b307fa9d
Cristian Goina <goinac@janelia.hhmi.org>
2017-10-20 11:45:47 -0400

    unit test for allocate bundle on new storage

--- jacsstorage-services/src/test/java/org/janelia/jacsstorage/service/DistributedStorageManagementServiceTest.java
+++ jacsstorage-services/src/test/java/org/janelia/jacsstorage/service/DistributedStorageManagementServiceTest.java
@@ -1,21 +1,28 @@
 package org.janelia.jacsstorage.service;
 
+import com.google.common.collect.ImmutableMap;
 import org.janelia.jacsstorage.dao.JacsBundleDao;
 import org.janelia.jacsstorage.dao.JacsStorageVolumeDao;
 import org.janelia.jacsstorage.model.jacsstorage.JacsBundle;
 import org.janelia.jacsstorage.model.jacsstorage.JacsBundleBuilder;
+import org.janelia.jacsstorage.model.jacsstorage.JacsStorageVolume;
 import org.janelia.jacsstorage.model.jacsstorage.JacsStorageVolumeBuilder;
 import org.janelia.jacsstorage.model.jacsstorage.StorageAgentInfo;
+import org.janelia.jacsstorage.model.support.SetFieldValueHandler;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.util.Optional;
+import java.util.function.Predicate;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -73,4 +80,47 @@ public class DistributedStorageManagementServiceTest {
         assertThat(dataBundle.getConnectionInfo(), equalTo("testConnectionInfo"));
     }
 
+    @Test
+    public void allocateStorageOnNewVolume() {
+        Long testBundleId = 10L;
+        Long testVolumeId = 20L;
+        String testLocation = "testLocation";
+        String testAgentURL = "http://agentURL";
+        String testAgentConnectionInfo = "agent:100";
+        String testAgentMountPoint = "/storage";
+        when(storageAgentManager.findRandomRegisteredAgent(any(Predicate.class)))
+                .thenReturn(Optional.of(new StorageAgentInfo(testLocation, testAgentURL, testAgentConnectionInfo, testAgentMountPoint)));
+        JacsStorageVolume testVolume = new JacsStorageVolumeBuilder().storageVolumeId(testVolumeId).location(testLocation).build();
+        when(storageVolumeDao.getStorageByLocationAndCreateIfNotFound(testLocation))
+                .thenReturn(testVolume);
+        doAnswer((invocation) -> {
+            JacsBundle bundle = invocation.getArgument(0);
+            bundle.setId(testBundleId);
+            return null;
+        }).when(bundleDao).save(any(JacsBundle.class));
+
+        JacsBundle testBundle = new JacsBundleBuilder().owner("anowner").name("aname").build();
+        Optional<JacsBundle> bundleResult = testStorageManagementService.allocateStorage(testBundle);
+        assertTrue(bundleResult.isPresent());
+        Mockito.verify(storageVolumeDao).update(testVolume, ImmutableMap.of(
+                "mountHostIP", new SetFieldValueHandler<>(testAgentConnectionInfo),
+                "mountPoint", new SetFieldValueHandler<>(testAgentMountPoint),
+                "mountHostURL", new SetFieldValueHandler<>(testAgentURL)
+        ));
+        bundleResult.ifPresent(dataBundle -> {
+            assertThat(dataBundle.getStorageVolumeId(), equalTo(testVolumeId));
+            assertThat(dataBundle.getConnectionInfo(), equalTo(testAgentConnectionInfo));
+            assertThat(dataBundle.getConnectionURL(), equalTo(testAgentURL));
+            assertTrue(dataBundle.getStorageVolume().isPresent());
+            dataBundle.getStorageVolume().ifPresent(v -> {
+                assertThat(v, equalTo(testVolume));
+            });
+            Mockito.verify(bundleDao).save(dataBundle);
+            assertThat(dataBundle.getId(), equalTo(testBundleId));
+            assertThat(dataBundle.getPath(), equalTo(testAgentMountPoint + "/" + testBundleId));
+            Mockito.verify(bundleDao).update(dataBundle, ImmutableMap.of(
+                    "path", new SetFieldValueHandler<>(dataBundle.getPath())
+            ));
+        });
+    }
 }
