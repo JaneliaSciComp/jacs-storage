6b5e549f25cbef167971e073f3302e30df961f18
Cristian Goina <goinac@janelia.hhmi.org>
2018-01-11 15:45:36 -0500

    build variables for listeners ports

--- jacsstorage-commonweb/src/main/install/etc/servicevars.sh
+++ jacsstorage-commonweb/src/main/install/etc/servicevars.sh
@@ -1,13 +1,14 @@
+DEBUG_OPTS=""
+
 JACSSTORAGE_CONFIG="${jacs.runtime.env.installDir}/etc/config.properties"
 JACSSTORAGE_LOG_CONFIG="${jacs.runtime.env.installDir}/etc/logback.xml"
 
-DEBUG_OPTS=""
 JAVA_OPTS="${DEBUG_OPTS} -Xmx2G -Xms512M -Dlogback.configurationFile=${JACSSTORAGE_LOG_CONFIG}"
 
 AGENT_HOST=`hostname -s`
-AGENT_HTTP_PORT=9881
-AGENT_TCP_PORT=11000
+AGENT_HTTP_PORT=${jacs.runtime.env.agentHttpPort}
+AGENT_TCP_PORT=${jacs.runtime.env.agentTcpPort}
 
 MASTER_IP=jade1
-MASTER_PORT=9880
+MASTER_PORT=${jacs.runtime.env.masterHttpPort}
 MASTER_URL="http://${MASTER_IP}:${MASTER_PORT}/jacsstorage/master_api/v1"
