a02a60998ae42a182bcf1526d1a27ee7e10769d3
Cristian Goina <goinac@janelia.hhmi.org>
2018-01-11 14:23:04 -0500

    config and run scripts for the masterweb service

---
+++ jacsstorage-commonweb/src/main/install/etc/servicevars.sh
@@ -0,0 +1,13 @@
+JACSSTORAGE_CONFIG="${installDir}/etc/config.properties"
+JACSSTORAGE_LOG_CONFIG="${installDir}/etc/logback.xml"
+
+DEBUG_OPTS=""
+JAVA_OPTS="${DEBUG_OPTS} -Xmx2G -Xms512M -Dlogback.configurationFile=${JACSSTORAGE_LOG_CONFIG}"
+
+AGENT_HOST=`hostname -s`
+AGENT_HTTP_PORT=9881
+AGENT_TCP_PORT=11000
+
+MASTER_IP=jade1
+MASTER_PORT=9880
+MASTER_URL="http://${MASTER_IP}:${MASTER_PORT}/jacsstorage/master_api/v1"
