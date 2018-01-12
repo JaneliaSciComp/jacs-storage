d934895ef683a8a443d8e81a415cd0e863e67eeb
Cristian Goina <goinac@janelia.hhmi.org>
2018-01-11 23:32:28 -0500

    set jacs config

--- jacsstorage-agentweb/src/main/install/scripts/run-service.sh
+++ jacsstorage-agentweb/src/main/install/scripts/run-service.sh
@@ -4,7 +4,7 @@ source "${jacs.runtime.env.installDir}/etc/servicevars.sh"
 
 cd "${jacs.runtime.env.installDir}"
 
-JAVA_OPTS="${JAVA_OPTS}" \
+JAVA_OPTS="${JAVA_OPTS}" JACSSTORAGE_CONFIG="${JACSSTORAGE_CONFIG}" \
 bin/jacsstorage-agentweb \
      -b 0.0.0.0 \
      -p ${AGENT_HTTP_PORT} \
--- jacsstorage-masterweb/src/main/install/scripts/run-service.sh
+++ jacsstorage-masterweb/src/main/install/scripts/run-service.sh
@@ -4,7 +4,7 @@ source "${jacs.runtime.env.installDir}/etc/servicevars.sh"
 
 cd "${jacs.runtime.env.installDir}"
 
-JAVA_OPTS="${JAVA_OPTS}" \
+JAVA_OPTS="${JAVA_OPTS}" JACSSTORAGE_CONFIG="${JACSSTORAGE_CONFIG}" \
 bin/jacsstorage-masterweb \
      -b 0.0.0.0 \
      -p ${MASTER_PORT} \
