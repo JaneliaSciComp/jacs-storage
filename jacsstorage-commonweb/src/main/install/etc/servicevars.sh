2c144e1f00fdbfc15b35514286b28c7f8b0361f0
Cristian Goina <goinac@janelia.hhmi.org>
2018-01-29 14:41:19 -0500

    renamed the master port var

--- jacsstorage-commonweb/src/main/install/etc/servicevars.sh
+++ jacsstorage-commonweb/src/main/install/etc/servicevars.sh
@@ -8,6 +8,6 @@ JAVA_OPTS="${DEBUG_OPTS} -Xmx2G -Xms512M -Dlogback.configurationFile=${JACSSTORA
 AGENT_HOST=`hostname -s`
 AGENT_HTTP_PORT=${jacs.runtime.env.agentHttpPort}
 
-MASTER_IP=jade1
-MASTER_PORT=${jacs.runtime.env.masterHttpPort}
-MASTER_URL="http://${MASTER_IP}:${MASTER_PORT}/jacsstorage/master_api/v1"
+MASTER_HOST=jade1
+MASTER_HTTP_PORT=${jacs.runtime.env.masterHttpPort}
+MASTER_URL="http://${MASTER_HOST}:${MASTER_HTTP_PORT}/jacsstorage/master_api/v1"
--- jacsstorage-masterweb/src/main/install/scripts/run-service.sh
+++ jacsstorage-masterweb/src/main/install/scripts/run-service.sh
@@ -7,5 +7,5 @@ cd "${jacs.runtime.env.installDir}"
 JAVA_OPTS="${JAVA_OPTS}" JACSSTORAGE_CONFIG="${JACSSTORAGE_CONFIG}" \
 bin/jacsstorage-masterweb \
      -b 0.0.0.0 \
-     -p ${MASTER_PORT} \
+     -p ${MASTER_HTTP_PORT} \
      $*
