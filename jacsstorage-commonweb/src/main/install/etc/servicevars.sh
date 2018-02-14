6c4a39f2c350a826e581fb190449f7f495540edc
Cristian Goina <goinac@janelia.hhmi.org>
2018-02-14 15:33:07 -0500

    master host configurable

--- jacsstorage-commonweb/src/main/install/etc/servicevars.sh
+++ jacsstorage-commonweb/src/main/install/etc/servicevars.sh
@@ -8,6 +8,6 @@ JAVA_OPTS="${DEBUG_OPTS} -Xmx2G -Xms512M -Dlogback.configurationFile=${JACSSTORA
 AGENT_HOST=`hostname -s`
 AGENT_HTTP_PORT=${jacs.runtime.env.agentHttpPort}
 
-MASTER_HOST=jade1
+MASTER_HOST=${jacs.runtime.env.masterHost}
 MASTER_HTTP_PORT=${jacs.runtime.env.masterHttpPort}
 MASTER_URL="http://${MASTER_HOST}:${MASTER_HTTP_PORT}/jacsstorage/master_api/v1"
