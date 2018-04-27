625ab542da1756a53b22b223e9426fca81f1a036
Cristian Goina <goinac@janelia.hhmi.org>
2018-04-27 16:30:24 -0400

    set debug env variable without passing it to the runtime

--- jacsstorage-agentweb/src/main/install/scripts/run-service.sh
+++ jacsstorage-agentweb/src/main/install/scripts/run-service.sh
@@ -4,6 +4,8 @@ source "${jacs.runtime.env.installDir}/etc/servicevars.sh"
 
 cd "${jacs.runtime.env.installDir}"
 
+DEBUG_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5006"
+
 JAVA_OPTS="${JAVA_OPTS}" JACSSTORAGE_CONFIG="${JACSSTORAGE_CONFIG}" \
 bin/jacsstorage-agentweb \
      -b 0.0.0.0 \
