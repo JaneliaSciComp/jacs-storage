62011e3a14a7aecab3aeb47f4957a7cbe1bdce0e
Cristian Goina <goinac@janelia.hhmi.org>
2018-06-21 15:05:54 -0400

    trying nio copy

--- jacsstorage-core/src/main/java/org/janelia/jacsstorage/coreutils/FileUtils.java
+++ jacsstorage-core/src/main/java/org/janelia/jacsstorage/coreutils/FileUtils.java
@@ -1,9 +1,12 @@
 package org.janelia.jacsstorage.coreutils;
 
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
-import java.nio.file.Files;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
 import java.nio.file.Path;
 
 public class FileUtils {
@@ -12,27 +15,39 @@ public class FileUtils {
 
     /**
      * Copies from a source path to a stream. The only reason for this is a larger buffer.
+     *
      * @param srcPath
      * @param dstStream
      * @return
      * @throws IOException
      */
     public static long copyFrom(Path srcPath, OutputStream dstStream)
-            throws IOException{
-        try (InputStream in = Files.newInputStream(srcPath)) {
-            return copy(in, dstStream);
+            throws IOException {
+        try (ReadableByteChannel in = new FileInputStream(srcPath.toFile()).getChannel()) {
+            return copy(in, Channels.newChannel(dstStream));
         }
     }
 
-    private static long copy(InputStream source, OutputStream sink)
-            throws IOException
-    {
+    private static long copy(ReadableByteChannel source, WritableByteChannel sink)
+            throws IOException {
         long nread = 0L;
-        byte[] buf = new byte[BUFFER_SIZE];
+        final ByteBuffer buffer = ByteBuffer.allocateDirect(BUFFER_SIZE);
         int n;
-        while ((n = source.read(buf)) > 0) {
-            sink.write(buf, 0, n);
+        while ((n = source.read(buffer)) > 0) {
             nread += n;
+            // prepare the buffer to be drained
+            buffer.flip();
+            // write to the channel, may block
+            sink.write(buffer);
+            // If partial transfer, shift remainder down
+            // If buffer is empty, same as doing clear()
+            buffer.compact();
+        }
+        // EOF will leave buffer in fill state
+        buffer.flip();
+        // make sure the buffer is fully drained.
+        while (buffer.hasRemaining()) {
+            sink.write(buffer);
         }
         return nread;
     }
