3dfb911743dcd13ca2f0cde9571ab3cfe4c40ce4
Cristian Goina <goinac@hhmi.org>
2018-09-28 15:06:37 -0400

    inject the request into the exception response handler

--- jacsstorage-commonweb/src/main/java/org/janelia/jacsstorage/rest/NotFoundRequestHandler.java
+++ jacsstorage-commonweb/src/main/java/org/janelia/jacsstorage/rest/NotFoundRequestHandler.java
@@ -4,7 +4,9 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
@@ -13,15 +15,20 @@ import javax.ws.rs.ext.Provider;
 public class NotFoundRequestHandler implements ExceptionMapper<NotFoundException> {
     private static final Logger LOG = LoggerFactory.getLogger(NotFoundRequestHandler.class);
 
+    @Context
+    private HttpServletRequest request;
+
     @Override
     public Response toResponse(NotFoundException exception) {
-        LOG.error("Invalid argument response", exception);
+        LOG.error("Path not found: {}", request.getRequestURI(), exception);
         String errorMessage = exception.getMessage();
-        return Response
+        Response.ResponseBuilder responseBuilder = Response
                 .status(Response.Status.NOT_FOUND)
-                .entity(new ErrorResponse(errorMessage))
-                .header("Content-Length", 0)
-                .build();
+                .entity(new ErrorResponse(errorMessage));
+        if (StringUtils.equalsAnyIgnoreCase("HEAD", request.getMethod())) {
+            responseBuilder.header("Content-Length", 0);
+        }
+        return responseBuilder.build();
     }
 
 }
