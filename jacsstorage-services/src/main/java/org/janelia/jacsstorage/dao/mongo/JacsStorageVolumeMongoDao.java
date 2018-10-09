67323efa870a721a7b68d2763e98a9445e1e4e85
Cristian Goina <goinac@hhmi.org>
2018-10-08 21:07:09 -0400

    trying a fix for potential null storageRootTemplate during migration

--- jacsstorage-services/src/main/java/org/janelia/jacsstorage/dao/mongo/JacsStorageVolumeMongoDao.java
+++ jacsstorage-services/src/main/java/org/janelia/jacsstorage/dao/mongo/JacsStorageVolumeMongoDao.java
@@ -180,9 +180,14 @@ public class JacsStorageVolumeMongoDao extends AbstractMongoDao<JacsStorageVolum
 
     private Bson createStorageRootBaseExpr() {
         Bson indexOfVarExpr = new Document("$indexOfCP", Arrays.asList("$storageRootTemplate", new Document("$literal", "$")));
-        return createCondExpr(createEqExpr(indexOfVarExpr, -1),
+        Bson indexOfVarWithDefaultExpr = ifNullExp(indexOfVarExpr, -1);
+        return createCondExpr(createEqExpr(indexOfVarWithDefaultExpr, -1),
                 "$storageRootTemplate",
                 createSubstrExpr("$storageRootTemplate", 0, indexOfVarExpr));
+    }
+
+    private Bson ifNullExp(Object expr, Object nullDefault) {
+        return new Document("ifNull", Arrays.asList(expr, nullDefault));
     }
 
     private Bson createCondExpr(Object cond, Object thenValue, Object elseValue) {
