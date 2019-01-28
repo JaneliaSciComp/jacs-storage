3f798fb6a2df1c3ef2cb1fe046aba47ace1a22fd
Cristian Goina <goinac@hhmi.org>
2019-01-28 09:06:54 -0500

    populate filter params

--- jacsstorage-agentweb/src/main/java/org/janelia/jacsstorage/rest/ContentFilterRequestHelper.java
+++ jacsstorage-agentweb/src/main/java/org/janelia/jacsstorage/rest/ContentFilterRequestHelper.java
@@ -1,14 +1,27 @@
 package org.janelia.jacsstorage.rest;
 
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.janelia.jacsstorage.io.ContentFilterParams;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.MultivaluedMap;
+import java.util.Collection;
 
 class ContentFilterRequestHelper {
     static ContentFilterParams createContentFilterParamsFromQuery(MultivaluedMap<String, String> queryParameters) {
         ContentFilterParams filterParams = new ContentFilterParams();
-        // TODO
+        queryParameters.forEach((k, vs) -> {
+            if ("filterType".equalsIgnoreCase(k)) {
+                if (CollectionUtils.isNotEmpty(vs))
+                    filterParams.setFilterType(vs.get(0));
+            } else if ("entryName".equalsIgnoreCase(k)) {
+                if (CollectionUtils.isNotEmpty(vs))
+                    filterParams.setEntryName(vs.get(0));
+            } else if (StringUtils.isNotBlank(k) && CollectionUtils.isNotEmpty(vs)) {
+                filterParams.addFilterTypeSpecificParam(k, vs.get(0));
+            }
+        });
         return filterParams;
     }
 }
