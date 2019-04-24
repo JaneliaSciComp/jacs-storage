c16fd39e97043b5c2f124933a33b3a09fc80453a
Cristian Goina <goinac@hhmi.org>
2019-04-24 17:27:03 -0400

    default exposed agent host to hostname

--- jacsstorage-agentweb/src/main/install/scripts/run-service.sh
+++ jacsstorage-agentweb/src/main/install/scripts/run-service.sh
@@ -5,7 +5,7 @@ source "${jacs.runtime.env.installDir}/etc/servicevars.sh"
 cd "${jacs.runtime.env.installDir}"
 
 DEBUG_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5006"
-EXPOSED_AGENT_HOST="${jacs.runtime.env.agentHost}"
+EXPOSED_AGENT_HOST=`hostname`
 
 JAVA_OPTS="${JAVA_OPTS}" JACSSTORAGE_CONFIG="${JACSSTORAGE_CONFIG}" \
 bin/jacsstorage-agentweb \
