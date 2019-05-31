1306e31c37d9ca2e7f7444932e8184ccda98c1ff
Cristian Goina <goinac@hhmi.org>
2019-05-31 15:40:10 -0400

    use toString

--- jacsstorage-services/src/main/java/org/janelia/jacsstorage/service/localservice/LocalStorageUsageManager.java
+++ jacsstorage-services/src/main/java/org/janelia/jacsstorage/service/localservice/LocalStorageUsageManager.java
@@ -186,7 +186,7 @@ public class LocalStorageUsageManager implements StorageUsageManager {
             if (LOG.isDebugEnabled()) {
                 LOG.warn("Storage path {} could not be resolved to a real path for user {} on volume {}", storagePath, username, storageVolume, e);
             } else {
-                LOG.warn("Storage path {} could not be resolved to a real path for user {} : {}", storagePath, username, e.getLocalizedMessage());
+                LOG.warn("Storage path {} could not be resolved to a real path for user {} : {}", storagePath, username, e.toString());
             }
             return null;
         }
