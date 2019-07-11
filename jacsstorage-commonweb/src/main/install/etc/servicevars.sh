a729f774c5d1e3973bbb4ba5fef84ff8eefd1932
Cristian Goina <goinac@hhmi.org>
2019-07-11 17:54:59 -0400

    use build property for setting agent host too

--- jacsstorage-commonweb/src/main/install/etc/servicevars.sh
+++ jacsstorage-commonweb/src/main/install/etc/servicevars.sh
@@ -5,7 +5,7 @@ JACSSTORAGE_LOG_CONFIG="${jacs.runtime.env.installDir}/etc/logback.xml"
 
 JAVA_OPTS="${DEBUG_OPTS} -Xmx2G -Xms512M -Dlogback.configurationFile=${JACSSTORAGE_LOG_CONFIG}"
 
-AGENT_HOST=`hostname`
+AGENT_HOST=${jacs.runtime.env.agentHost}
 AGENT_HTTP_PORT=${jacs.runtime.env.agentHttpPort}
 
 MASTER_HOST=${jacs.runtime.env.masterHost}
