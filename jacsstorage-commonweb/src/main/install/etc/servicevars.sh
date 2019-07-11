855cd0ff338c0696dff4fd7e9549507bcc969f6f
Cristian Goina <goinac@hhmi.org>
2019-07-11 17:50:56 -0400

    set master host from config instead of using hostname

--- jacsstorage-commonweb/src/main/install/etc/servicevars.sh
+++ jacsstorage-commonweb/src/main/install/etc/servicevars.sh
@@ -8,6 +8,6 @@ JAVA_OPTS="${DEBUG_OPTS} -Xmx2G -Xms512M -Dlogback.configurationFile=${JACSSTORA
 AGENT_HOST=`hostname`
 AGENT_HTTP_PORT=${jacs.runtime.env.agentHttpPort}
 
-MASTER_HOST=`hostname`
+MASTER_HOST=${jacs.runtime.env.masterHost}
 MASTER_HTTP_PORT=${jacs.runtime.env.masterHttpPort}
 MASTER_URL="http://${MASTER_HOST}:${MASTER_HTTP_PORT}/jacsstorage/master_api/v1"
