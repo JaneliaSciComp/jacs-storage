a0896686859bed64a8f82b7248be6839cf3ae0d7
Cristian Goina <goinac@hhmi.org>
2024-10-14 14:24:42 -0400

    ignore unknow pros in jaderesults

--- jacsstorage-clients/src/main/java/org/janelia/jacsstorage/clients/api/JadeResults.java
+++ jacsstorage-clients/src/main/java/org/janelia/jacsstorage/clients/api/JadeResults.java
@@ -2,6 +2,9 @@ package org.janelia.jacsstorage.clients.api;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class JadeResults<T> {
     private List<T> resultList;
 
